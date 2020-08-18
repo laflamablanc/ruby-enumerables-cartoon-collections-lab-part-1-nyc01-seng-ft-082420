2a5f361ed94d5e8c24a7b45abcfaaef812295bcb
Sean LaFlam <sean.laflam@gmail.com>
2020-08-18 19:15:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |ele, idx|
-    puts idx.to_s + ". " + ele
+    puts (idx.to_s+1) + ". " + ele
   end
 end
